1f7d9ef1cc2b670ae2cc578097a60b0046792012
pureheroky <arseniygol2017@gmail.com>
2024-08-20 00:48:39 +0300

    ver 1.0

---
+++ main/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"pureheroky.com/mongodbsetup"
+)
+
+var mongoClient *mongo.Client
+
+func main() {
+	var err error
+	mongoClient, err = setupMongodbClient()
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer mongoClient.Disconnect(context.Background())
+
+	r := gin.Default()
+	r.Use(corsMiddleware())
+
+	r.GET("/getuservalue/:valuetype", getUserValueHandler)
+	r.GET("/getimage/:collname/:queryname", getImageHandler)
+	r.GET("/getproject/:projectid", getProjectHandler)
+	r.GET("/uploadimage/:query/:id/:imagename", uploadImageHandler)
+	r.POST("/projects", createProjectHandler)
+
+	r.Run("localhost:8080")
+}
+
+func getEnvValue(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func setupMongodbClient() (*mongo.Client, error) {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	mongouri := getEnvValue("MONGOURI")
+	opts := options.Client().ApplyURI(mongouri).SetServerAPIOptions(serverAPI)
+	return mongo.Connect(context.Background(), opts)
+}
+
+func getUserValueHandler(c *gin.Context) {
+	value := c.Param("valuetype")
+	coll := mongoClient.Database("pureweb").Collection("client")
+	data := mongodbsetup.GetUserValue(coll, "pureheroky", value)
+	c.JSON(http.StatusOK, gin.H{"data": data, "status": http.StatusOK})
+}
+
+func getImageHandler(c *gin.Context) {
+	coll := mongoClient.Database("pureweb").Collection(c.Param("collname"))
+	imageData, contentType, err := mongodbsetup.GetImage(coll, c.Param("queryname"))
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Document not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve image"})
+		}
+		return
+	}
+
+	c.Header("Content-Type", contentType)
+	c.Writer.Write(imageData)
+}
+
+func getProjectHandler(c *gin.Context) {
+	prjId := c.Param("projectid")
+	coll := mongoClient.Database("pureweb").Collection("projects")
+	data := mongodbsetup.GetProject(coll, prjId)
+	c.JSON(http.StatusOK, gin.H{"data": data, "status": http.StatusOK})
+}
+
+func uploadImageHandler(c *gin.Context) {
+	id := c.Param("id")
+	query := c.Param("query")
+	image := c.Param("imagename")
+	coll := mongoClient.Database("pureweb").Collection(query)
+	err := mongodbsetup.SaveImageInDB(coll, id, "temp/"+image)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createProjectHandler(c *gin.Context) {
+	var project mongodbsetup.Project
+	if err := c.BindJSON(&project); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	coll := mongoClient.Database("pureweb").Collection("projects")
+	_, err := coll.InsertOne(context.Background(), project)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, project)
+}
+
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
---
+++ mongodbsetup/mongo.go
@@ -0,0 +1,146 @@
+package mongodbsetup
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+	"unicode"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type User struct {
+	ID       string   `json:"_id"`
+	Age      int64    `json:"age"`
+	Gitlink  string   `json:"gitlink"`
+	Image    string   `json:"image"`
+	Username string   `json:"name"`
+	Tglink   string   `json:"tglink"`
+	Status   bool     `json:"status"`
+	Skills   []string `json:"skills"`
+}
+
+type Skills struct {
+	Skills []string `json:"skills"`
+}
+
+type Project struct {
+	ID          string             `json:"id"`
+	Name        string             `json:"name"`
+	PrjGitlink  string             `json:"prjgitlink"`
+	PrjImage    string             `json:"image"`
+	PrjStatus   string             `json:"prjstatus"`
+	PrjComplete string             `json:"prjcomplete"`
+	PrjDate     string             `json:"prjdate"`
+	PrjDesc     string             `json:"prjDesc"`
+	PrjWeblink  string             `json:"prjweblink"`
+}
+
+func capitalize(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	runes := []rune(s)
+	runes[0] = unicode.ToUpper(runes[0])
+	return string(runes)
+}
+
+func SaveImageInDB(coll *mongo.Collection, id, imagePath string) error {
+	fileData, err := os.ReadFile(imagePath)
+	if err != nil {
+		return err
+	}
+
+	base64Data := base64.StdEncoding.EncodeToString(fileData)
+
+	filter := bson.M{"id": id}
+	update := bson.M{"$set": bson.M{"image": base64Data}}
+	_, err = coll.UpdateOne(context.TODO(), filter, update)
+	return err
+}
+
+func getFieldValue(v interface{}, key string) string {
+	rv := reflect.ValueOf(v)
+	keys := strings.Split(key, ".")
+
+	for _, k := range keys {
+		k = capitalize(k)
+		rv = reflect.Indirect(rv).FieldByName(k)
+		if !rv.IsValid() {
+			fmt.Printf("No such field: %s in obj\n", k)
+			return ""
+		}
+	}
+
+	return fmt.Sprintf("%v", rv.Interface())
+}
+
+func GetImage(coll *mongo.Collection, id string) ([]byte, string, error) {
+	var result bson.M
+	filter := bson.D{{Key: "id", Value: id}}
+
+	err := coll.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		return nil, "", err
+	}
+
+	imageData, ok := result["image"].(string)
+	if !ok {
+		return nil, "", errors.New("image not found or invalid type")
+	}
+
+	decodedImage, err := base64.StdEncoding.DecodeString(imageData)
+	if err != nil {
+		return nil, "", err
+	}
+
+	contentType := "image/png"
+
+	return decodedImage, contentType, nil
+}
+
+func GetUserValue(coll *mongo.Collection, title string, key string) string {
+	var result bson.M
+	err := coll.FindOne(context.TODO(), bson.D{{Key: "name", Value: title}}).Decode(&result)
+
+	if err != nil {
+		panic(err)
+	}
+
+	user := &result
+	jsonData, err := json.MarshalIndent(user, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+
+	var resStruct User
+
+	err = json.Unmarshal(jsonData, &resStruct)
+	if err != nil {
+		fmt.Printf("Error unmarshalling json: %v\n", err)
+		panic(err)
+	}
+	return getFieldValue(resStruct, key)
+}
+
+func GetProject(coll *mongo.Collection, id string) *bson.M {
+	var result bson.M
+	err := coll.FindOne(context.TODO(), bson.D{{Key: "id", Value: id}}).Decode(&result)
+
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No project was found with the id %s\n", id)
+		return nil
+	}
+
+	if err != nil {
+		panic(err)
+	}
+	return &result
+}
+
